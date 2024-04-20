6fa8bf495d6534994276f0b3c200086845649f32
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-04-20 14:16:12 -0300

    chore: add default variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96",
+        primaryDark: "#58E6D9",
+      },
+    },
   },
   plugins: [],
 };
