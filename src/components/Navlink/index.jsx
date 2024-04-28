35911f66fd8948dc10ecd1eab491c2557097c57b
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-04-28 16:33:45 -0300

    feat: add indicator to show current page

--- src/components/Navlink/index.jsx
+++ src/components/Navlink/index.jsx
@@ -1,10 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Navlink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  const currentPath = router.asPath;
+
   return (
-    <Link className={className} href={href}>
+    <Link className={`${className} relative group`} href={href}>
       {title}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          currentPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
